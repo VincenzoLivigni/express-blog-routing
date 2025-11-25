0181d45316e7039a78d43299c4f2d37a9e79fc3b
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-25 14:58:41 +0100

    add posts routes to index.js

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+const postsRouter = require("./routes/posts")                   // importo router dei post
+
+app.use(express.static("public"))
+
 app.get("/", (req, res) => {
     res.send("Welcome to my blog")
 })
+
+app.use("/posts", postsRouter)                                  // tutte le richieste a "/posts/.." verranno gestite da "postsRouter"
 
 app.listen(port, () => {
     console.log(`Example app ${port}`);
